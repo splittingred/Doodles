9e897ab3ea24620fa2698ee5a2a402a594fade27
Shaun McCormick <shaun@modx.com>
2011-01-27 12:37:53 -0600

    Removing deprecated line

--- core/components/doodles/controllers/mgr/header.php
+++ core/components/doodles/controllers/mgr/header.php
@@ -9,7 +9,6 @@ $modx->regClientStartupScript($doodles->config['jsUrl'].'mgr/doodles.js');
 $modx->regClientStartupHTMLBlock('<script type="text/javascript">
 Ext.onReady(function() {
     Doodles.config = '.$modx->toJSON($doodles->config).';
-    Doodles.config.connector_url = "'.$doodles->config['connectorUrl'].'";
 });
 </script>');
 
