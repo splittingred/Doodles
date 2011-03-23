3f43d89ff9ad762e5f6c4f90e8131461a7d85f16
dafyk <dafyk@inmail.sk>
2011-03-23 07:53:51 -0700

    Fix for hardcoded "Invalid data" message

--- core/components/doodles/processors/mgr/doodle/updatefromgrid.php
+++ core/components/doodles/processors/mgr/doodle/updatefromgrid.php
@@ -4,9 +4,9 @@
  * @subpackage processors
  */
 /* parse JSON */
-if (empty($scriptProperties['data'])) return $modx->error->failure('Invalid data.');
+if (empty($scriptProperties['data'])) return $modx->error->failure($modx->lexicon('doodles.doodle_err_data'));
 $_DATA = $modx->fromJSON($scriptProperties['data']);
-if (!is_array($_DATA)) return $modx->error->failure('Invalid data.');
+if (!is_array($_DATA)) return $modx->error->failure($modx->lexicon('doodles.doodle_err_data'));
 
 /* get obj */
 if (empty($_DATA['id'])) return $modx->error->failure($modx->lexicon('doodles.doodle_err_ns'));
